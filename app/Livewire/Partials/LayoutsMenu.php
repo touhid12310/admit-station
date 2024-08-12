ce5e413b082531f0e5366f7ea4978926e4aab10c
mannanshihab <amannanshihab@gmail.com>
2024-08-12 20:47:12 +0600

    partials

---
+++ app/Livewire/Partials/LayoutsMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Partials;
+
+use Livewire\Component;
+
+class LayoutsMenu extends Component
+{
+    public function render()
+    {
+        return view('livewire.partials.layouts-menu');
+    }
+}
---
+++ resources/views/livewire/partials/layouts-footer.blade.php
@@ -0,0 +1,115 @@
+<div>
+    <!-- footer-area-start -->
+    <footer>
+        <div class="tp-footer-4">
+        <div class="tp-footer-4-bg tp-footer-inner">
+            <div class="container">
+                <div class="tp-footer-4-top">
+                    <div class="row">
+                    <div class="col-lg-6">
+                        <h4 class="tp-footer-4-top-title">Sign now for offers <br> and keep yourself informed!</h4>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="tp-footer-4-top-form">
+                            <form action="#">
+                                <input type="text" placeholder="Enter your email...">
+                            <button class="tp-footer-4-top-btn">Send
+                                <span>
+                                    <svg width="16" height="12" viewBox="0 0 16 12" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M10.0029 1L15.0029 6L10.0029 11" stroke="currentColor" stroke-width="1.5"
+                                            stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M1 6H14.8633" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10"
+                                            stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="tp-footer-4-main pt-90 pb-70">
+                    <div class="row">
+                    <div class="col-lg-2 col-md-6">
+                        <div class="tp-footer-widget tp-footer-4-col-1 mb-30">
+                            <div class="tp-footer-4-btn tp-footer-btn-inner">
+                                <a class="active" href="university-apply.html">Apply Now</a>
+                                <a href="contact.html">Contact Us</a>
+                                <a href="contact.html">Visit Us</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="tp-footer-widget tp-footer-widget-4 has-border tp-footer-4-col-2 mb-30">
+                            <div class="tp-footer-widget-link">
+                                <ul>
+                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="#">Courses</a></li>
+                                <li><a href="#">Parent Portal</a></li>
+                                <li><a href="#">News & Blogs</a></li>
+                                <li><a href="#">Events</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="tp-footer-widget tp-footer-widget-4 tp-footer-4-col-3 mb-30">
+                            <div class="tp-footer-widget-link">
+                                <ul>
+                                <li><a href="#">Students</a></li>
+                                <li><a href="#">Addmition</a></li>
+                                <li><a href="#">Faculty & Staffs</a></li>
+                                <li><a href="#">Tuition & Fees</a></li>
+                                <li><a href="#">Dress Code</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="tp-footer-widget tp-footer-widget-4 tp-footer-4-col-4 mb-30">
+                            <div class="tp-footer-4-info">
+                                <span>Germany — <br> 785 15h Street, Office 478 Berlin, De 81566</span>
+                                <div class="tp-footer-4-mail">
+                                <a href="mailto:info@email.com">info@email.com</a>
+                                </div>
+                                <div class="tp-footer-4-phone">
+                                <a href="tel:0123456789">+(402) 763 282 46</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tp-footer-4-bottom tp-footer-inner-bottom">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-3 col-lg-2 col-md-3">
+                    <div class="tp-footer-4-logo tp-header-logo">
+                        <a href="#"><img src="assets/img/logo/logo-white.png" alt=""></a>
+                    </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-7 col-md-5">
+                    <div class="tp-footer-4-copyright text-start text-md-center">
+                        <span>Copyright © 2024 <a href="#">ThemePure</a></span>
+                        <span><a href="#">All Rights Reserved</a></span>
+                        <span><a href="#">Privacy & Policy</a></span>
+                    </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4">
+                    <div class="tp-footer-4-social text-md-end">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
+    <!-- footer-area-end -->
+</div>
